f3c13d98c824533de4f40505e191a03590ab5a64
OlgaKrzy <89542@student.ue.poznan.pl>
2023-04-24 12:54:15 +0200

    exercise 7 update - in the original code was a shadow name

--- ex_7.py
+++ ex_7.py
@@ -3,23 +3,11 @@
 # jesli sie nie da, to zwroc False
 # jesli sie da, zwroc True
 
-def wykres(wykres) -> bool:
-    if len(wykres) < 2:
+def wykres(dane) -> bool:
+    if len(dane) < 2:
         return False
-
-    x1, y1 = wykres[0]
-    x2, y2 = wykres[1]
-    if x2 - x1 == 0:
-        slope = None
-    else:
-        slope = (y2 - y1) / (x2 - x1)
-
-    for i in range(2, len(wykres)):
-        x1, y1 = wykres[i - 1]
-        x2, y2 = wykres[i]
-        if x2 - x1 == 0:
-            if slope is not None:
-                return False
-        elif slope is None or (y2 - y1) / (x2 - x1) != slope:
+    slope = [dane[1][0] - dane[0][0], dane[1][1] - dane[0][1]]
+    for i in range(1, len(dane)):
+        if [dane[i][0] - dane[i-1][0], dane[i][1] - dane[i-1][1]] != slope:
             return False
     return True
